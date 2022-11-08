3615702b7f075c23040ae2e38d01447526edea19
Cicada <202586563@qq.com>
2022-11-08 23:29:23 +0800

    login

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,7 +3,15 @@ import Vue from 'vue'
 import {
   Select,
   Option,
+  Input,
+  Button,
+  Form,
+  FormItem,
 } from 'element-ui'
 
 Vue.use(Select)
 Vue.use(Option)
+Vue.use(Input)
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,16 +1,42 @@
 <template>
   <div class="login_container">
     <div class="login_content">
-      <div class="title">Login</div>
-      <!-- <el-select v-model="areaValue" placeholder="请选择">
-        <el-option
-          v-for="item in areaCodes"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value"
-        >
-        </el-option>
-      </el-select> -->
+      <div class="title">{{ !isLogon ? '登录' : '注册' }}</div>
+      <el-form
+        :model="loginForm"
+        :rules="rules"
+        ref="ruleForm"
+        label-width="100px"
+        class="demo-ruleForm"
+      >
+        <!-- 手机号 -->
+        <el-form-item label="账号" prop="phone">
+          <el-input
+            v-model="loginForm.phone"
+            placeholder="手机号"
+            prop="phone"
+            oninput="value=value.replace(/[^\d]/g,'');if(value.length > 11)value = value.slice(0, 11)"
+          />
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item label="密码" prop="password">
+          <el-input
+            v-model="loginForm.password"
+            placeholder="密码"
+            show-password
+          />
+        </el-form-item>
+        <!-- 确认密码 -->
+        <el-form-item v-if="isLogon" label="确认密码" prop="asPassword">
+          <el-input
+            v-model="loginForm.asPassword"
+            placeholder="确认密码"
+            show-password
+          />
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" @click="login">{{ !isLogon ? '登录' : '注册' }}</el-button>
+      <div class="switch" @click="switchType">{{ !isLogon ? '注册' : '登录' }}</div>
     </div>
   </div>
 </template>
@@ -19,27 +45,27 @@
 export default {
   data() {
     return {
-      areaValue: 'CN',
-      // 电话地域信息
-      areaCodes: [
-        {
-          value: 'CN',
-          label: '中国 +86',
-        },
-        {
-          value: 'RU',
-          label: '俄罗斯 +7',
-        },
-        {
-          value: 'JP',
-          label: '日本 +81',
-        },
-        {
-          value: 'US',
-          label: '美国 +1',
-        },
-      ],
+      isLogon: false,
+      loginForm: {
+        phone: '',
+        password: '',
+        asPassword: '',
+      },
+      rules: {
+        phone: [
+          { required: true, message: '手机号不能为空', trigger: 'blur' },
+          { min: 11, message: '手机号格式不正确', trigger: 'blur' },
+        ],
+        password: [
+          {}
+        ]
+      },
     }
+  },
+  methods: {
+    switchType() {
+      this.isLogon = !this.isLogon
+    },
   },
 }
 </script>
@@ -63,6 +89,11 @@ export default {
     .title {
       font-size: 30px;
       text-align: center;
+    }
+
+    .switch {
+      float: right;
+      cursor: pointer;
     }
   }
 }
